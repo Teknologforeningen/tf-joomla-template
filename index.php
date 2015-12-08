3ac35d8442a3f22b707f1a177f663f95fe55c424
Oskar Ehnstrom <oskar@ehnstrom.fi>
2015-12-08 14:16:13 +0200

    Refactor grid layout with flex grid

--- index.php
+++ index.php
@@ -2,67 +2,75 @@
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" >
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<jdoc:include type="head" />
+  <jdoc:include type="head" />
 
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
 
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/foundation/css/foundation.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/foundation/css/foundation.css" type="text/css" />
 
-<!-- Style tag to allow joomla template paths via php -->
-<style>
-@font-face {
-  font-family: 'Montserrat-Bold';
-  src:
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.eot?#iefix') format('embedded-opentype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.otf')  format('opentype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.woff') format('woff'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.ttf')  format('truetype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.svg#Montserrat-Bold') format('svg');
-  font-weight: normal;
-  font-style: normal;
-}
+  <!-- Style tag to allow joomla template paths via php -->
+  <style>
+  @font-face {
+    font-family: 'Montserrat-Bold';
+    src:
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.eot?#iefix') format('embedded-opentype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.otf')  format('opentype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.woff') format('woff'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.ttf')  format('truetype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Bold.svg#Montserrat-Bold') format('svg');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-@font-face {
-  font-family: 'Montserrat-Light';
-  src:
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.eot?#iefix') format('embedded-opentype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.otf')  format('opentype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.woff') format('woff'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.ttf')  format('truetype'),
-    url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.svg#Montserrat-Light') format('svg');
-  font-weight: normal;
-  font-style: normal;
-}
-</style>
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/style.css" type="text/css" />
+  @font-face {
+    font-family: 'Montserrat-Light';
+    src:
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.eot?#iefix') format('embedded-opentype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.otf')  format('opentype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.woff') format('woff'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.ttf')  format('truetype'),
+      url('<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/fonts/Montserrat-Light.svg#Montserrat-Light') format('svg');
+    font-weight: normal;
+    font-style: normal;
+  }
+  </style>
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/style.css" type="text/css" />
 
 </head>
 <body class="site">
 
+<!-- Title bar for small screens -->
 <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
   <button class="menu-icon" type="button" data-toggle></button>
   <div class="title-bar-title">Teknologföreningen</div>
 </div>
 
-<div class="top-bar" id="main-menu">
-  <div class="top-bar-left">
-    <img id="menu-logo" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/img/tf_natside_logon-08.svg" alt="TF">
-  </div>
-  <div class="top-bar-right">
-    <jdoc:include type="modules" name="topmenu" />
-  </div>
-</div>
-
-<div id="top-module" class="row">
-  <div class="small-12 columns">
-    <jdoc:include type="modules" name="top" />
+<!-- Top bar and menu for large screens -->
+<div id="top-bar-wrapper">
+  <div class="top-bar row align-middle" id="main-menu">
+    <div class="column middle-4 large-4">
+      <img id="menu-logo" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/img/tf_logo_black.svg" alt="TF">
+    </div>
+    <div class="column middle-8 large-8 justify-right">
+      <jdoc:include type="modules" name="topmenu" />
+    </div>
   </div>
 </div>
 
+<!-- Module place right below the top-bar -->
+<div id="top-module-wrapper">
+  <div id="top-module" class="row">
+    <div class="small-12 columns">
+      <jdoc:include type="modules" name="top" />
+    </div>
+  </div>
+</div>
+
+<!-- Main content area -->
 <div class="row site-content">
   <div class="small-12 medium-4 large-3 columns">
     <jdoc:include type="modules" name="left" />
@@ -75,27 +83,39 @@
   </div>
 </div>
 
-<footer class="footer row">
-  <div id="footer-left" class="small-12 medium-6 large-6 columns">
-    <jdoc:include type="modules" name="footer-left" />
-  </div>
-  <div id="footer-right" class="small-12 medium-6 large-6 columns">
-    <jdoc:include type="modules" name="footer-right" />
-  </div>
-</footer>
 
+<!-- Footer -->
+<div id="footer-wrapper">
+  <footer class="footer row">
+    <div id="footer-left" class="small-12 medium-6 large-6 columns">
+      <jdoc:include type="modules" name="footer-left" />
+    </div>
+    <div id="footer-right" class="small-12 medium-6 large-6 columns">
+      <jdoc:include type="modules" name="footer-right" />
+    </div>
+  </footer>
+</div>
 
+<!-- Load scripts here to make sure everything else is loaded before it -->
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/foundation/js/vendor/jquery.min.js"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/foundation/js/foundation.min.js"></script>
 <script>
   $(document).foundation();
 
+  /*
+   * Joomla insists on importing one ancient version of bootstrap, but new foundation needs a later version.
+   */
+
   jQuery.prototype.tooltip = function(){
-    console.log("Tooltip called for but not present due to jQuery version conflict.");
+    if (console && console.log) {
+      console.log("Tooltip called for but not present due to jQuery version conflict.");
+    }
   };
 
   jQuery.prototype.popover = function(){
-    console.log("Popover called for but not present due to jQuery version conflict.");
+    if (console && console.log) {
+      console.log("Popover called for but not present due to jQuery version conflict.");
+    }
   };
 </script>
 </body>
